604cea0033af28bcb85f7446d39dca1b7a5c1122
Trent Albright <trent.albright@gmail.com>
2017-03-14 16:46:31 -0400

    trace like grpc

--- plugin/event_bus.go
+++ plugin/event_bus.go
@@ -1,7 +1,9 @@
 package plugin
 
 import (
+	"fmt"
 	"io"
+	"runtime"
 	"sync"
 
 	pb "github.com/talbright/keds/gen/proto"
@@ -23,20 +25,21 @@ type EventBusMember struct {
 	EventBus IEventBusAdapter
 	Stream   pb.KedsService_EventBusServer
 	quitc    chan struct{}
-	trlog    trace.EventLog
+	events   trace.EventLog
 }
 
 func NewEventBusMember(eb IEventBusAdapter, stream pb.KedsService_EventBusServer) *EventBusMember {
+	_, file, line, _ := runtime.Caller(1)
 	return &EventBusMember{
 		EventBus: eb,
 		Stream:   stream,
 		quitc:    make(chan struct{}),
-		trlog:    trace.NewEventLog("plugin.EventBusMember", "anonymous"),
+		events:   trace.NewEventLog("plugin.EventBusMember", fmt.Sprintf("%s:%d", file, line)),
 	}
 }
 
 func (m *EventBusMember) Receive(ctx context.Context, event *pb.PluginEvent) (err error) {
-	m.trlog.Printf("forwarding event to plugin: %v", event)
+	m.events.Printf("forwarding event to plugin: %v", event)
 	return m.Stream.Send(event)
 }
 
@@ -45,13 +48,13 @@ func (m *EventBusMember) Listen(ctx context.Context) (quitc chan struct{}, err e
 	go func() {
 		for {
 			if in, err := m.Stream.Recv(); err == nil {
-				m.trlog.Printf("Listen: event received from plugin: %v", in)
+				m.events.Printf("Listen: event received from plugin: %v", in)
 			} else if err == io.EOF {
-				m.trlog.Printf("Listen: EOF")
+				m.events.Printf("Listen: EOF")
 				close(m.quitc)
 				return
 			} else {
-				m.trlog.Errorf("Listen: error from stream recv: %v", err)
+				m.events.Errorf("Listen: error from stream recv: %v", err)
 				close(m.quitc)
 				return
 			}
@@ -63,19 +66,20 @@ func (m *EventBusMember) Listen(ctx context.Context) (quitc chan struct{}, err e
 type EventBus struct {
 	memberLock *sync.RWMutex
 	members    []IEventBusMember
-	trlog      trace.EventLog
+	events     trace.EventLog
 }
 
 func NewEventBus() *EventBus {
+	_, file, line, _ := runtime.Caller(0)
 	return &EventBus{
 		memberLock: &sync.RWMutex{},
 		members:    make([]IEventBusMember, 0),
-		trlog:      trace.NewEventLog("plugin.EventBus", "singleton"),
+		events:     trace.NewEventLog("plugin.EventBus", fmt.Sprintf("%s:%d", file, line)),
 	}
 }
 
 func (b *EventBus) AddStream(ctx context.Context, stream pb.KedsService_EventBusServer) (quitc chan struct{}, err error) {
-	b.trlog.Printf("adding new stream")
+	b.events.Printf("adding new stream")
 	member := NewEventBusMember(b, stream)
 	if err = b.appendMember(member); err == nil {
 		quitc, err = member.Listen(ctx)
@@ -86,7 +90,7 @@ func (b *EventBus) AddStream(ctx context.Context, stream pb.KedsService_EventBus
 func (b *EventBus) Publish(ctx context.Context, event *pb.PluginEvent) (err error) {
 	b.memberLock.RLock()
 	defer b.memberLock.RUnlock()
-	b.trlog.Printf("publishing event '%s' to %d members", event.Name, len(b.members))
+	b.events.Printf("publishing event '%s' to %d members", event.Name, len(b.members))
 	for _, member := range b.members {
 		member.Receive(ctx, event)
 	}
