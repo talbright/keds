5d7c8ab2d12e667a2c34927cb3d84154445af1d2
Trent Albright <trent.albright@gmail.com>
2017-12-07 17:46:27 -0500

    WIP: lost state

---
+++ plugin/builtin/notifications/message_test.go
@@ -0,0 +1,12 @@
+package main_test
+
+import (
+	. "github.com/talbright/keds/plugin/builtin/notifications"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Message", func() {
+
+})
